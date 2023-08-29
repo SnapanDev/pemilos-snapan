a14580c26b66cda562f43bf1a6ce926a5e16aff3
akmmp241 <akm.mp24@gmail.com>
2023-08-29 21:12:08 +0700

    update sidebar

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -9,15 +9,10 @@
 
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
 
-{{--  @vite('resources/css/app.css')--}}
   <link rel="stylesheet" href="{{ asset('/build/assets/app.9c91aa94.css') }}">
-
-{{--  @vite('resources/js/app.js')--}}
 
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
   <script src="{{ asset('js/init-alpine.js') }}"></script>
-{{--  <script src="{{ asset('build/assets/app.334e7359.js') }}"></script>--}}
-
   @yield('assets')
 </head>
 
@@ -181,6 +176,26 @@
                   d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"/>
               </svg>
               <span class="ml-4">User</span>
+            </a>
+          </li>
+
+          <li class="relative px-6 py-3">
+            @if (str_contains(request()->url(), route('admin.candidates.index')))
+              <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                    aria-hidden="true"></span>
+            @endif
+            <a
+              class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 {{ str_contains(request()->url(), route('admin.settings.index')) ? 'text-gray-800  dark:text-gray-100' : '' }}"
+              href="{{ route('admin.settings.index') }}">
+              <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                   stroke-linejoin="round"
+                   stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <path
+                  d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"/>
+                <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+              </svg>
+
+              <span class="ml-4">Settings</span>
             </a>
           </li>
 
